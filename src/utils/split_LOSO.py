f59e3539aef99fa60f26075d3da151bd35166c44
yiannislevy <yiannis.levy@gmail.com>
2024-01-09 23:59:26 +0200

    Update script for LOSO to get already processed data ready for CNN

--- src/utils/split_LOSO.py
+++ src/utils/split_LOSO.py
@@ -1,27 +1,35 @@
 import pandas as pd
 import os
-import pickle
+import pickle as pkl
 
 with open ("../../data/FIC.pkl", "rb") as f:
+    FIC = pkl.load(f)
+
+# Opened the processed dataset
+with open ("../../data/processed.nosync/all_final/all_final.pkl", "rb") as f:
     dataset = pkl.load(f)
 
-df = pd.DataFrame(dataset)
+subject_ids = FIC["subject_id"]
+subject_to_indices = {subject: [] for subject in set(subject_ids)}
+for i, subject_id in enumerate(subject_ids):
+    subject_to_indices[subject_id].append(i)
 
-# Extract unique subjects
-unique_subjects = df['subject_id'].unique()
-
-for subject in unique_subjects:
+for subject in set(subject_ids):
+    # Define directory name
     directory = f"../../data/LOSO/all_but_{subject}"
     os.makedirs(directory, exist_ok=True)
 
-    train_indices = df[df['subject_id'] != subject].index
-    test_indices = df[df['subject_id'] == subject].index
+    # Get training and testing indices
+    train_indices = [i for subj, indices in subject_to_indices.items() if subj != subject for i in indices]
+    test_indices = subject_to_indices[subject]
 
-    train_signals = [dataset['signals_proc'][i] for i in train_indices]
-    test_signals = [dataset['signals_proc'][i] for i in test_indices]
+    # Select data for training and testing
+    train_data = [dataset[i] for i in train_indices]
+    test_data = [dataset[i] for i in test_indices]
 
+    # Save to .pkl files
     with open(f"{directory}/train_data.pkl", 'wb') as f:
-        pickle.dump(train_signals, f)
+        pkl.dump(train_data, f)
 
     with open(f"{directory}/test_data.pkl", 'wb') as f:
-        pickle.dump(test_signals, f)
+        pkl.dump(test_data, f)

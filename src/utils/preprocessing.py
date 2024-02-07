5433ab4d1a0b7f63b12b2051d328ae397e302b13
yiannislevy <yiannis.levy@gmail.com>
2024-02-07 21:28:53 +0100

    Add functions to load data by session and mildly adapt previous functions

--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -28,8 +28,48 @@ def standardize_data(data, json_file):
 
 # Example usage:
 # standardized_data = standardize_data(your_data_structure, 'mean_std_values.json')
+def load_data(test_subject, subject_to_indices, path_to_data):
+    training_data, testing_data = [], []
+    training_labels, testing_labels = [], []
 
-def load_split_data(test_subject, subject_to_indices):
+    # Load data by sessions_old based on subject_to_indices mapping
+    for subject, sessions in subject_to_indices.items():
+        subject_data, subject_labels = [], []
+        for session_id in sessions:
+            session_data, session_labels = load_session_data(f"{path_to_data}/session_{session_id}.pkl")
+            subject_data.append(session_data)
+            subject_labels.append(session_labels)
+
+        # Aggregate data for each subject
+        subject_data = np.concatenate(subject_data, axis=0)
+        subject_labels = np.concatenate(subject_labels, axis=0)
+
+        # Distribute data into training or testing based on subject ID
+        if str(subject) == str(test_subject):
+            testing_data.append(subject_data)
+            testing_labels.append(subject_labels)
+        else:
+            training_data.append(subject_data)
+            training_labels.append(subject_labels)
+
+    # Combine all training and testing data and labels
+    training_data = np.concatenate(training_data, axis=0)
+    training_labels = np.concatenate(training_labels, axis=0)
+    testing_data = np.concatenate(testing_data, axis=0)
+    testing_labels = np.concatenate(testing_labels, axis=0)
+
+    return training_data, training_labels, testing_data, testing_labels
+
+
+def load_session_data(path):
+    data = pd.read_pickle(path)
+    signal_data = np.array([item[0] for item in data])
+    signal_data = signal_data[:, :, 1:]  # Exclude the timestamps column
+    label_data = np.array([item[1] for item in data])
+    return signal_data, label_data
+
+
+def load_data_subjects(test_subject, subject_to_indices):
     training_data, testing_data, validation_data = [], [], []
     training_labels, testing_labels, validation_labels = [], [], []
 
@@ -38,13 +78,13 @@ def load_split_data(test_subject, subject_to_indices):
         if subject != test_subject :
             # and subject != validation_subject:
             subject_data, subject_labels = load_subject_data(f"../data/ProcessedSubjects/MajorityLabel/subjects/subject_{subject}/data.pkl")
-            # subject_data, subject_labels = load_subject_data(f"../data/ProcessedSubjects/Standardized/subject_{subject}/data.pkl") #TODO re-eval
+            # subject_data, subject_labels = load_subject_data(f"../data/ProcessedSubjects/std_1/subject_{subject}/data.pkl") #TODO re-eval
             training_data.append(subject_data)
             training_labels.append(subject_labels)
 
     # Load testing data (only the test subject)
     test_data, test_labels = load_subject_data(f"../data/ProcessedSubjects/MajorityLabel/subjects/subject_{test_subject}/data.pkl")
-    # test_data, test_labels = load_subject_data(f"../data/ProcessedSubjects/Standardized/subject_{test_subject}/data.pkl")
+    # test_data, test_labels = load_subject_data(f"../data/ProcessedSubjects/std_1/subject_{test_subject}/data.pkl")
     testing_data.append(test_data)
     testing_labels.append(test_labels)
 
